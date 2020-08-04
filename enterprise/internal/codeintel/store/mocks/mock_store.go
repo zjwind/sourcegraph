068de15e0784bd2296846570fb1682df825c004d
Eric Fritz <eric@eric-fritz.com>
2020-08-04 11:10:38 -0500

    Regenerate mocks.

--- enterprise/internal/codeintel/store/mocks/mock_store.go
+++ enterprise/internal/codeintel/store/mocks/mock_store.go
@@ -60,6 +60,9 @@ type MockStore struct {
 	// GetDumpByIDFunc is an instance of a mock function object controlling
 	// the behavior of the method GetDumpByID.
 	GetDumpByIDFunc *StoreGetDumpByIDFunc
+	// GetDumpForCommitFunc is an instance of a mock function object
+	// controlling the behavior of the method GetDumpForCommit.
+	GetDumpForCommitFunc *StoreGetDumpForCommitFunc
 	// GetIndexByIDFunc is an instance of a mock function object controlling
 	// the behavior of the method GetIndexByID.
 	GetIndexByIDFunc *StoreGetIndexByIDFunc
@@ -243,6 +246,11 @@ func NewMockStore() *MockStore {
 		},
 		GetDumpByIDFunc: &StoreGetDumpByIDFunc{
 			defaultHook: func(context.Context, int) (store.Dump, bool, error) {
+				return store.Dump{}, false, nil
+			},
+		},
+		GetDumpForCommitFunc: &StoreGetDumpForCommitFunc{
+			defaultHook: func(context.Context, int, string, string, string) (store.Dump, bool, error) {
 				return store.Dump{}, false, nil
 			},
 		},
@@ -474,6 +482,9 @@ func NewMockStoreFrom(i store.Store) *MockStore {
 		},
 		GetDumpByIDFunc: &StoreGetDumpByIDFunc{
 			defaultHook: i.GetDumpByID,
+		},
+		GetDumpForCommitFunc: &StoreGetDumpForCommitFunc{
+			defaultHook: i.GetDumpForCommit,
 		},
 		GetIndexByIDFunc: &StoreGetIndexByIDFunc{
 			defaultHook: i.GetIndexByID,
@@ -2139,6 +2150,127 @@ func (c StoreGetDumpByIDFuncCall) Args() []interface{} {
 // Results returns an interface slice containing the results of this
 // invocation.
 func (c StoreGetDumpByIDFuncCall) Results() []interface{} {
+	return []interface{}{c.Result0, c.Result1, c.Result2}
+}
+
+// StoreGetDumpForCommitFunc describes the behavior when the
+// GetDumpForCommit method of the parent MockStore instance is invoked.
+type StoreGetDumpForCommitFunc struct {
+	defaultHook func(context.Context, int, string, string, string) (store.Dump, bool, error)
+	hooks       []func(context.Context, int, string, string, string) (store.Dump, bool, error)
+	history     []StoreGetDumpForCommitFuncCall
+	mutex       sync.Mutex
+}
+
+// GetDumpForCommit delegates to the next hook function in the queue and
+// stores the parameter and result values of this invocation.
+func (m *MockStore) GetDumpForCommit(v0 context.Context, v1 int, v2 string, v3 string, v4 string) (store.Dump, bool, error) {
+	r0, r1, r2 := m.GetDumpForCommitFunc.nextHook()(v0, v1, v2, v3, v4)
+	m.GetDumpForCommitFunc.appendCall(StoreGetDumpForCommitFuncCall{v0, v1, v2, v3, v4, r0, r1, r2})
+	return r0, r1, r2
+}
+
+// SetDefaultHook sets function that is called when the GetDumpForCommit
+// method of the parent MockStore instance is invoked and the hook queue is
+// empty.
+func (f *StoreGetDumpForCommitFunc) SetDefaultHook(hook func(context.Context, int, string, string, string) (store.Dump, bool, error)) {
+	f.defaultHook = hook
+}
+
+// PushHook adds a function to the end of hook queue. Each invocation of the
+// GetDumpForCommit method of the parent MockStore instance inovkes the hook
+// at the front of the queue and discards it. After the queue is empty, the
+// default hook function is invoked for any future action.
+func (f *StoreGetDumpForCommitFunc) PushHook(hook func(context.Context, int, string, string, string) (store.Dump, bool, error)) {
+	f.mutex.Lock()
+	f.hooks = append(f.hooks, hook)
+	f.mutex.Unlock()
+}
+
+// SetDefaultReturn calls SetDefaultDefaultHook with a function that returns
+// the given values.
+func (f *StoreGetDumpForCommitFunc) SetDefaultReturn(r0 store.Dump, r1 bool, r2 error) {
+	f.SetDefaultHook(func(context.Context, int, string, string, string) (store.Dump, bool, error) {
+		return r0, r1, r2
+	})
+}
+
+// PushReturn calls PushDefaultHook with a function that returns the given
+// values.
+func (f *StoreGetDumpForCommitFunc) PushReturn(r0 store.Dump, r1 bool, r2 error) {
+	f.PushHook(func(context.Context, int, string, string, string) (store.Dump, bool, error) {
+		return r0, r1, r2
+	})
+}
+
+func (f *StoreGetDumpForCommitFunc) nextHook() func(context.Context, int, string, string, string) (store.Dump, bool, error) {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
+
+	if len(f.hooks) == 0 {
+		return f.defaultHook
+	}
+
+	hook := f.hooks[0]
+	f.hooks = f.hooks[1:]
+	return hook
+}
+
+func (f *StoreGetDumpForCommitFunc) appendCall(r0 StoreGetDumpForCommitFuncCall) {
+	f.mutex.Lock()
+	f.history = append(f.history, r0)
+	f.mutex.Unlock()
+}
+
+// History returns a sequence of StoreGetDumpForCommitFuncCall objects
+// describing the invocations of this function.
+func (f *StoreGetDumpForCommitFunc) History() []StoreGetDumpForCommitFuncCall {
+	f.mutex.Lock()
+	history := make([]StoreGetDumpForCommitFuncCall, len(f.history))
+	copy(history, f.history)
+	f.mutex.Unlock()
+
+	return history
+}
+
+// StoreGetDumpForCommitFuncCall is an object that describes an invocation
+// of method GetDumpForCommit on an instance of MockStore.
+type StoreGetDumpForCommitFuncCall struct {
+	// Arg0 is the value of the 1st argument passed to this method
+	// invocation.
+	Arg0 context.Context
+	// Arg1 is the value of the 2nd argument passed to this method
+	// invocation.
+	Arg1 int
+	// Arg2 is the value of the 3rd argument passed to this method
+	// invocation.
+	Arg2 string
+	// Arg3 is the value of the 4th argument passed to this method
+	// invocation.
+	Arg3 string
+	// Arg4 is the value of the 5th argument passed to this method
+	// invocation.
+	Arg4 string
+	// Result0 is the value of the 1st result returned from this method
+	// invocation.
+	Result0 store.Dump
+	// Result1 is the value of the 2nd result returned from this method
+	// invocation.
+	Result1 bool
+	// Result2 is the value of the 3rd result returned from this method
+	// invocation.
+	Result2 error
+}
+
+// Args returns an interface slice containing the arguments of this
+// invocation.
+func (c StoreGetDumpForCommitFuncCall) Args() []interface{} {
+	return []interface{}{c.Arg0, c.Arg1, c.Arg2, c.Arg3, c.Arg4}
+}
+
+// Results returns an interface slice containing the results of this
+// invocation.
+func (c StoreGetDumpForCommitFuncCall) Results() []interface{} {
 	return []interface{}{c.Result0, c.Result1, c.Result2}
 }
 
