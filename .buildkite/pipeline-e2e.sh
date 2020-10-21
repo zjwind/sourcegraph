7839eba0a3b6b1b58ffcf88740c96c365ada72bc
Dave Try <dave@sourcegraph.com>
2020-10-21 17:40:56 -0500

    generate pipeline to ensure the same agent is used for all steps

---
+++ .buildkite/pipeline-e2e.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+# Outputs a pipeline that targets agents that have the same 'name' meta-data
+# value as the step that does the pipeline upload. This means that all the
+# steps will run on the same agent machine, assuming that the 'name' meta-data
+# value is unique to each agent.
+#
+# Each agent needs to be configured with meta-data like so:
+#
+# meta-data="name=<unique-name>"
+#
+# To use, save this file as .buildkite/pipeline.sh, chmod +x, and then set your
+# first pipeline step to run this and pipe it into pipeline upload:
+#
+# .buildkite/pipeline.sh | buildkite-agent pipeline upload
+#
+
+name=$(buildkite-agent meta-data get name)
+
+cat << EOF
+
+env:
+  ENTERPRISE: '1'
+  DOCKER_BUILDKIT: '1'
+  FORCE_COLOR: '3'
+  GO111MODULE: 'on'
+  IMAGE: 'server_instrumented'
+  TAG: '${BUILDKITE_BUILD_NUMBER}_${BUILDKITE_RETRY_COUNT}'
+  COVERAGE_INSTRUMENT: 'true'
+  VAGRANT_RUN_ENV: 'CI'
+  VAGRANT_DOTFILE_PATH: "/var/lib/buildkite/builds/$BUILDKITE_AGENT_NAME/vagrant.d"
+
+steps:
+- artifact_paths: ./*.png;./e2e.mp4;./ffmpeg.log
+  # setting to pass until tests are 100% confirmed as working, so as to avoid disruting dev workflow on main
+  command:
+    - .buildkite/test.sh sourcegraph-e2e || true
+  timeout_in_minutes: 20
+  label: ':docker::arrow_right::chromium:'
+    agents:
+      name: "$name"
+EOF
